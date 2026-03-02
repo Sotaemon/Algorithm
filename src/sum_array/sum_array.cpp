c49fb0f14a9867ac3361e689fc8197b5ef085ad1
Sotaemon <sotaemon_wang@foxmail.com>
2026-03-02 21:22:34 +0800

    matrix

--- src/sum_array/sum_array.cpp
+++ src/sum_array/sum_array.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
-int sum_array(int n, int a[]) {
+int
+sum_array(int n, int a[]) {
     int sum = 0;
     for (int i = 0; i < n; ++i) {
         sum += a[i];
@@ -8,8 +9,9 @@ int sum_array(int n, int a[]) {
     return sum;
 }
 
-int main(void) {
-    int a[] = {2, 5, 4, 0, 4};
+int
+main(void) {
+    int a[] = { 2, 5, 4, 0, 4 };
     int n = sizeof(a) / sizeof(int);
     int sum = sum_array(n, a);
     std::cout << sum << std::endl;
